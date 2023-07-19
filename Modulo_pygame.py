37934e1040be12e42191b6e3921a15a5235ca11c
fernandogentilp <gentilcx1@gmail.com>
2023-07-19 16:40:49 -0300

    Primeiro projeto em Pygame

---
+++ Modulo_pygame.py
@@ -0,0 +1,6 @@
+import pygame
+from obj import pular
+
+#Interessante, não sabia que era só criar um arquivo contendo uma função e importar ela em outro arquivo
+#estando na mesma pasta!
+pular()
---
+++ obj.py
@@ -0,0 +1,3 @@
+def pular():
+    print("Pula")
+
